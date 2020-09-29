ac4c976632fbc2165587077a71e7e90659124d50
Andrew Suh <andrewsuh98@gmail.com>
2020-09-28 21:41:34 -0400

    Allow movement of the tank by calling tank.move() on the draw() function.

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const tank = new Tank(canvas.width / 2 - 25, canvas.height - 60, 50, 50)
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   tank.draw(ctx);
+  tank.move(canvas.width);
   window.requestAnimationFrame(draw);
 }
 
