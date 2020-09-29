867ddeb6b9eac0007ad7d9bfdda1b05238262dea
Andrew Suh <andrewsuh98@gmail.com>
2020-09-28 22:09:01 -0400

    Add event listener to missile, however, the key handlers are not implemented yet.

--- model/Missile.js
+++ model/Missile.js
@@ -7,6 +7,25 @@ class Missile extends Sprite {
     constructor(x, y, width, height) {
         super(x, y, width, height, image);
         this.dy = -10;
+        document.addEventListener("keydown", this.keyDownHandler.bind(this));
+        document.addEventListener("keyup", this.keyUpHandler.bind(this));
+    }
+
+    keyDownHandler(e) {
+        // if (e.key === "Right" || e.key === "ArrowRight") {
+        //     this.dx = 7;
+        // } else if (e.key === "Left" || e.key === "ArrowLeft") {
+        //     this.dx = -7;
+        // }
+        // TODO: implement what to do when space bar is pressed.
+    }
+
+    keyUpHandler(e) {
+        // if (e.key === "Right" || e.key === "ArrowRight") {
+        //     this.dx = 0;
+        // } else if (e.key === "Left" || e.key === "ArrowLeft") {
+        //     this.dx = 0;
+        // }
     }
 
     move(canvasWidth) {
