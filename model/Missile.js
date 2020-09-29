b1014aee850b5e3341afe192c6f747ba7053165b
Andrew Suh <andrewsuh98@gmail.com>
2020-09-28 21:56:57 -0400

    Create Missile class, implement it with the missile image, then update the move() to have constant upwards speed.

---
+++ model/Missile.js
@@ -0,0 +1,17 @@
+import Sprite from "./Sprite.js";
+
+const image = new Image(15, 15);
+image.src = "./assets/missile.png";
+
+class Missile extends Sprite {
+    constructor(x, y, width, height) {
+        super(x, y, width, height, image);
+        this.dy = -10;
+    }
+
+    move(canvasWidth) {
+        super.move(0, this.dy);
+    }
+}
+
+export default Missile;
--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 import Tank from "./model/Tank.js";
+import Missile from "./model/Missile.js";
 
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
-const tank = new Tank(canvas.width / 2 - 25, canvas.height - 60, 50, 50)
+const tank = new Tank(canvas.width / 2 - 25, canvas.height - 60, 50, 50);
+
+const missile = new Missile(canvas.width / 2 - 7, canvas.height - 60, 15, 15);
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   tank.draw(ctx);
   tank.move(canvas.width);
+  missile.draw(ctx);
+  missile.move(canvas.width);
   window.requestAnimationFrame(draw);
 }
 
