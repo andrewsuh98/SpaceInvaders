9efa8098f6ff0b46f580ec5e5f6a7cb04fd5166e
Andrew Suh <andrewsuh98@gmail.com>
2020-09-29 19:41:24 -0400

    Create an array of missiles. Add function to create a new missile when space is pressed. Draw function draws and moves every element in the missiles array.

--- model/Missile.js
+++ model/Missile.js
@@ -1,31 +1,12 @@
 import Sprite from "./Sprite.js";
 
-const image = new Image(15, 15);
+const image = new Image(16, 16);
 image.src = "./assets/missile.png";
 
 class Missile extends Sprite {
     constructor(x, y, width, height) {
         super(x, y, width, height, image);
         this.dy = -10;
-        document.addEventListener("keydown", this.keyDownHandler.bind(this));
-        document.addEventListener("keyup", this.keyUpHandler.bind(this));
-    }
-
-    keyDownHandler(e) {
-        // if (e.key === "Right" || e.key === "ArrowRight") {
-        //     this.dx = 7;
-        // } else if (e.key === "Left" || e.key === "ArrowLeft") {
-        //     this.dx = -7;
-        // }
-        // TODO: implement what to do when space bar is pressed.
-    }
-
-    keyUpHandler(e) {
-        // if (e.key === "Right" || e.key === "ArrowRight") {
-        //     this.dx = 0;
-        // } else if (e.key === "Left" || e.key === "ArrowLeft") {
-        //     this.dx = 0;
-        // }
     }
 
     move(canvasWidth) {
--- script.js
+++ script.js
@@ -7,21 +7,31 @@ const ctx = canvas.getContext("2d");
 
 const tank = new Tank(canvas.width / 2 - 25, canvas.height - 60, 50, 50);
 
-const missile = new Missile(canvas.width / 2 - 7, canvas.height - 60, 15, 15);
+//const missile = new Missile(canvas.width / 2 - 7, canvas.height - 60, 15, 15);
 
 const invader = new Invader(canvas.width / 2 - 20, 60, 40, 40);
+
+const missiles = [];
+//document.addEventListener("keyup", this.keyUpHandler.bind(this));
+
+function keyDownHandler(e) {
+  if (e.key === " ") {
+    missiles.push(new Missile(tank.x + 25 - 8, canvas.height - 60 - 16, 16, 16))
+  }
+}
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   tank.draw(ctx);
   tank.move(canvas.width);
-  missile.draw(ctx);
-  missile.move(canvas.width);
+  missiles.forEach((missile) => {
+    missile.draw(ctx);
+    missile.move(canvas.width);
+  });
   invader.draw(ctx);
   invader.move(canvas.width);
   window.requestAnimationFrame(draw);
 }
 
 draw();
-
-//testing github configuration
+document.addEventListener("keydown", keyDownHandler);
