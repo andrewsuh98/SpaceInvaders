dafe8c6e2a3f866424dfbe42a75c9742d6826003
Andrew Suh <andrewsuh98@gmail.com>
2020-09-29 21:16:13 -0400

    Create invaders in random locations, in random intervals.

--- script.js
+++ script.js
@@ -14,6 +14,10 @@ const missiles = [];
 const invaders = [];
 invaders.push(new Invader(canvas.width / 2 - 20, 60, 40, 40)); // TODO remove
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
 function keyDownHandler(e) {
   if (e.key === " ") {
     missiles.push(new Missile(tank.x + 25 - 8, canvas.height - 60 - 16, 16, 16))
@@ -44,10 +48,18 @@ function updateInvaders() {
   });
 }
 
+function createRandomInvaders() {
+  const random = getRandomInt(canvas.width * 100);
+  if (random > 0 && random < canvas.width - 40){
+    invaders.push(new Invader(random, 0, 40, 40))
+  }
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   tank.draw(ctx);
   tank.move(canvas.width);
+  createRandomInvaders();
   updateMissiles();
   updateInvaders();
   window.requestAnimationFrame(draw);
